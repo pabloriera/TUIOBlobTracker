f6ec1514bc83fc4bf02d4a598103d9709f4d6166
miles <miles@Cynar.(none)>
2013-02-01 03:14:40 -0300

    6

--- TUIOBlobTracker.py
+++ TUIOBlobTracker.py
@@ -48,174 +48,188 @@ def near_blob(blobs,x,y,th):
 				
 				ans.append(cc)
 	return ans	
-										
 
-class Blob:
-	def __init__(self):
-		self.xpos = 0
-		self.ypos = 0
-		self.state = False
-		
-# START SCRIPT
-try:
-	#~ target = liblo.Address('10.42.0.46',12345)
-	target = liblo.Address('192.168.0.100',12345)
-	print "Connected"
+def main():										
 
-except liblo.AddressError, err:
-	print str(err)
-	sys.exit()
-
-# SEND INIT
-liblo.send(target, "/init")
-
-# STARTS TUIO
-tracking = tuio.Tracking()
-print "loaded profiles:", tracking.profiles.keys()
-print "list functions to access tracked objects:", tracking.get_helpers()
-
-
-
-#~ THERSHOLDS FOR GETTING OUT AND COLISSION
-out_th = 0.05
-col_th = 0.05
-
-id2b = {}
-b2id = {}
-
-#~ BLOB LIST
-blobs = []
-
-#~ NUMBER OF BLOBS
-N = 5
-
-for c in range(0,N):
-	blobs.append(Blob())
-	b2id[c]=0
-
-idlistOLD = []
-
-printFlag = True
-
-try:
-	while 1:
-
-		tracking.update()
-
-		idlist = []
-		pos_id = {}
-		
-		
-		#~ FILL IDLIST WITH LAST UPDATES
-		for cur in tracking.cursors():
-						
-			idlist.append(cur.sessionid)
-			pos_id[cur.sessionid] = (cur.xpos,cur.ypos)
-
-		idlist.sort()
-
-		#~ IF NOT EMPTY LIST
-		if idlist!=[] or idlistOLD:
-
-			#~ CHANGES BETWEEN OLD AND NEW
-			if idlist != idlistOLD:
-				#~ print "CHANGE"
-				
-				#~ ASK MISSING ID'S
-				idlistMISS = check_list_miss(idlistOLD,idlist)
-				
-				for l in idlistMISS:
-
-					c = id2b[l]
+	class Blob:
+		def __init__(self):
+			self.xpos = 0
+			self.ypos = 0
+			self.state = False
 			
-					#~ IT'S GETTING OUT
-					near_wall_flag = near_wall(blobs[c].xpos,blobs[c].ypos,out_th)
-					
-					#~ HAS MERGE WITH OTHERS?
-					near_blob_list = near_blob(b,blobs[c].xpos,blobs[c].ypos,col_th)
+	# START SCRIPT
+	try:
+		#~ target = liblo.Address('10.42.0.46',12345)
+		target = liblo.Address('192.168.0.100',12345)
+		print "Connected"
+
+	except liblo.AddressError, err:
+		print str(err)
+		sys.exit()
+
+	# SEND INIT
+	liblo.send(target, "/init")
+
+	# STARTS TUIO
+	tracking = tuio.Tracking()
+	print "loaded profiles:", tracking.profiles.keys()
+	print "list functions to access tracked objects:", tracking.get_helpers()
+
+
+
+	#~ THERSHOLDS FOR GETTING OUT AND COLISSION
+	out_th = 0.05
+	col_th = 0.05
+
+	id2b = {}
+	b2id = {}
+
+	#~ BLOB LIST
+	blobs = []
+
+	#~ NUMBER OF BLOBS
+	N = 5
+
+	for c in range(0,N):
+		blobs.append(Blob())
+		b2id[c]=0
+
+	idlistOLD = []
+
+	printFlag = True
+
+	try:
+		while 1:
+
+			tracking.update()
+
+			idlist = []
+			pos_id = {}
 			
-					
-					if len(near_blob_list)>1 and not(near_wall_flag):
-						#~ print "MERGE",near_blob_list
-						#~ SEARCH WICH ID REMAINS AND ATE THE MISSING
-						
-						for cc in near_blob_list:
-							if idlist.count(b2id[cc]) and c!=cc:
-									b2id[c] = b2id[cc]
-					
-					
-					else:	#~ DISAPPEARED				
-						#~ print "GO"
-						blobs[c].state = False
-						id2b.pop(l)
-						b2id[c] = 0
-						liblo.send(target, "/blob"+str(c+1)+ "/state", 0) # SEND BLOB OFF
-				
-				
-				#~ NEW IDs
-				
-				idlistNEW = check_list_new(idlistOLD,idlist)
-				
-				for l in idlistNEW:
-					
-					near_wall_flag = near_wall(pos_id[l][0],pos_id[l][1],out_th)
-					
-					near_blob_list = near_blob(b,pos_id[l][0], pos_id[l][1],col_th)
-					
-					#~ BLOB DIV
-					if len(near_blob_list)>1 and not(near_wall_flag):
-						
-						for c in near_blob_list:
+			
+			#~ FILL IDLIST WITH LAST UPDATES
+			for cur in tracking.cursors():
 							
-							if not(idlistNEW.count(b2id[c])):
-								
-								id2b[l] = c
-								b2id[c] = l
-								
-								break
-								
+				idlist.append(cur.sessionid)
+				pos_id[cur.sessionid] = (cur.xpos,cur.ypos)
+
+			idlist.sort()
+
+			#~ IF NOT EMPTY LIST, OR IDLISTOLD EMPTY
+			if idlist!=[] or idlistOLD:
+
+				#~ CHANGES BETWEEN OLD AND NEW
+				if idlist != idlistOLD:
+									
+					#~ ASK MISSING ID'S
+					idlistMISS = check_list_miss(idlistOLD,idlist)
 					
-					else: #~ CAME FROM OUTSIDE
+					for l in idlistMISS:
+
+						c = id2b[l]
+				
+						#~ IT'S GETTING OUT
+						near_wall_flag = near_wall(blobs[c].xpos,blobs[c].ypos,out_th)
 						
-						for c in range(N):
+						#~ HAS MERGE WITH OTHERS?
+						near_blob_list = near_blob(blobs,blobs[c].xpos,blobs[c].ypos,col_th)
+				
+						if len(near_blob_list)>1 and not(near_wall_flag):
+
+							#~ SEARCH WICH ID REMAINS AND MERGE THE MISSING
+							
+							for cc in near_blob_list:
+								if idlist.count(b2id[cc]) and c!=cc:
+										b2id[c] = b2id[cc]
 						
-							if not(blobs[c].state):
-								
-								id2b[l] = c
-								b2id[c] = l
-								
-								blobs[c].state = True
-								
-								liblo.send(target, "/blob"+str(c+1)+ "/state", 1) # SEND BLOB ON
-								break
+						
+						else:	#~ DISAPPEARED
+						
+							blobs[c].state = False
+							id2b.pop(l)
+							b2id[c] = 0
+							liblo.send(target, "/blob"+str(c+1)+ "/state", 0) # SEND BLOB OFF
 					
-
-			for c in range(N):
-				if blobs[c].state:
-					l = b2id[c]
-					blobs[c].xpos = pos_id[l][0]
-					blobs[c].ypos = pos_id[l][1]
-					liblo.send(target, "/blob" + str(c+1) + "/pos", blobs[c].xpos,blobs[c].ypos)
-
-
-		idlistOLD = idlist		
-
-		if printFlag:
-			#~ TEXTO PA PRINT
-			s1 = '\r'+str(idlist)
-			s2 = '\r'
-			
-			for c in range(N):
-				if blobs[c].state:
-					s2 = s2  + "#" + str(c+1) + " " + "%.2f" % blobs[c].xpos + " " + "%.2f" % blobs[c].ypos + "\t"
-				else:
-					s2 = s2  + "#" + str(c+1) + " " + "OFF" + "\t\t"
-
-			sys.stdout.write("\r                                                                ")		
-			sys.stdout.write(s1 + "\n")		
-			sys.stdout.write("\r                                                                ")		
-			sys.stdout.write(s2)
-			sys.stdout.write("\033[F")
+					
+					#~ NEW IDs
+					
+					idlistNEW = check_list_new(idlistOLD,idlist)
+					
+					for l in idlistNEW:
 						
-except KeyboardInterrupt:
-    tracking.stop()
+						near_wall_flag = near_wall(pos_id[l][0],pos_id[l][1],out_th)
+						
+						near_blob_list = near_blob(blobs,pos_id[l][0], pos_id[l][1],col_th)
+						
+						#~ BLOB DIV
+						if len(near_blob_list)>1 and not(near_wall_flag):
+							
+							for c in near_blob_list:
+								
+								if not(idlistNEW.count(b2id[c])):
+									
+									#~ REASSIGN IDENTITY
+									
+									id2b[l] = c
+									b2id[c] = l
+									
+									break
+									
+						
+						else: #~ CAME FROM OUTSIDE
+							
+							for c in range(N):
+							
+								if not(blobs[c].state):
+									
+									id2b[l] = c
+									b2id[c] = l
+									
+									blobs[c].state = True
+									
+									liblo.send(target, "/blob"+str(c+1)+ "/state", 1) # SEND BLOB ON
+									break
+						
+				#~ SEND ALL POSITIONS
+				for c in range(N):
+					if blobs[c].state:
+						l = b2id[c]
+						blobs[c].xpos = pos_id[l][0]
+						blobs[c].ypos = pos_id[l][1]
+						liblo.send(target, "/blob" + str(c+1) + "/pos", blobs[c].xpos,blobs[c].ypos)  # SEND POSITION
+
+
+			idlistOLD = idlist		
+
+			if printFlag:
+				#~ TEXT PRINT
+				s1 = '\r'+str(idlist)
+				s2 = '\r'
+				
+				for c in range(N):
+					if blobs[c].state:
+						s2 = s2  + "#" + str(c+1) + " " + "%.2f" % blobs[c].xpos + " " + "%.2f" % blobs[c].ypos + "\t"
+					else:
+						s2 = s2  + "#" + str(c+1) + " " + "OFF" + "\t\t"
+
+				sys.stdout.write("\r                                                                ")		
+				sys.stdout.write(s1 + "\n")		
+				sys.stdout.write("\r                                                                ")		
+				sys.stdout.write(s2)
+				sys.stdout.write("\033[F")
+							
+				
+	except KeyboardInterrupt:
+		tracking.stop()
+	except:
+		tracking.stop()
+		main()
+	
+		
+		
+if __name__ == "__main__":
+    main()
+	
+	
+	
+	
